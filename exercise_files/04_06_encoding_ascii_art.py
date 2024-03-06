f9bf6082e0a86ae3fd23a5b6c1eb28a93f0fd13f
Greg Geiger <greg.e.geiger@gmail.com>
2024-03-06 13:27:57 -0600

    Initial commit

---
+++ exercise_files/04_06_encoding_ascii_art.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# 04_06 Challenge
+# 2024-02-15:geg
+'''
+LinkedIn Learning
+Python Essential Training
+Ryan Mitchell
+Released: 1/25/2023
+
+04 06 Challenge
+Given ascii art encode into a list of tuples
+
+
+
+'''
+
+def encodeString(stringVal):
+    '''
+    Given a string return a list of tuples
+    AAAAABBBBAAA => [('A', 5), ('B', 4), ('A', 3)]
+    This is called Run Length Encoding
+
+    Parameters: 
+
+    Returns: List of 2 element tuples
+    '''
+    code_list = []
+
+    last_character = stringVal[0]
+    char_count = 1
+
+    for index in range(0, len(stringVal)):
+        # examine the character at index position
+        index_character = stringVal[index]
+        print(index_character)
+
+        # Check the character at index
+        # If it matches last_character just increment the character count
+        if index_character == last_character:
+            # If it matches last_character just increment the character count
+            char_count += 1
+        else:
+            # If does not match
+            # Add a tuple from prior run
+            code_list.append((last_character, char_count))
+            # Change the last_character to character at this index
+            last_character = index_character
+            # Reset the character count
+            char_count = 1
+        
+    # Add last tuple
+    code_list.append((last_character, char_count))
+
+        
+    return code_list
+
+
+
+def decodeString(encodedData):
+    '''
+    Given a string return a list of tuples
+    AAAAABBBBAAA => [('A', 5), ('B', 4), ('A', 3)]
+    This is called Run Length Encoding
+    '''
+    pass
+        
+    return None
+
+def main():
+    '''
+    main function
+    '''
+
+    art = '''
+
+
+
+                                   %%%%%%%%%%%%%%%%%%%                              
+                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                       
+                        %%%%%%%%                         %%%%%%%%                   
+                    %%%%%%%                                   %%%%%%                
+                  %%%%%%                                         %%%%%%             
+               %%%%%%                                               %%%%%           
+              %%%%%                                                   %%%%%         
+            %%%%%                                                       %%%%%       
+           %%%%                 %%%%%              %%%%%                  %%%%      
+          %%%%                 %%%%%%%            %%%%%%%                  %%%%     
+         %%%%                  %%%%%%%            %%%%%%%                   %%%%    
+        %%%%                   %%%%%%%            %%%%%%%                    %%%%   
+        %%%%                    %%%%%              %%%%%                     %%%%   
+       %%%%                                                                   %%%%  
+       %%%%                                                                   %%%%  
+       %%%%                                                                   %%%%  
+       %%%%                                                      %%%%        %%%%   
+        %%%%       %%%%%%                                        %%%%%       %%%%   
+        %%%%         %%%%                                       %%%%        %%%%    
+         %%%%         %%%%                                     %%%%         %%%%    
+          %%%%         %%%%%                                  %%%%         %%%%     
+           %%%%%         %%%%%                             %%%%%         %%%%%      
+            %%%%%          %%%%%%                        %%%%%          %%%%        
+              %%%%%           %%%%%%%               %%%%%%%           %%%%%         
+                %%%%%             %%%%%%%%%%%%%%%%%%%%%             %%%%%           
+                  %%%%%%%                                        %%%%%              
+                     %%%%%%%                                 %%%%%%%                
+                         %%%%%%%%%                     %%%%%%%%%                    
+                              %%%%%%%%%%%%%%%%%%%%%%%%%%%%%                         
+                                       %%%%%%%%%%%%                                 
+
+
+
+    '''
+
+
+    code_list = encodeString('AAAABBBAA')
+    print(code_list)
+
+
+
+
+if __name__ == "__main__":
+    main()
